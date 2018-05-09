0d0c9daa9d5f7192507c364ba677c5636e86eaf1
Israel Newham <israel.newham@gmail.com>
2018-05-09 18:26:26 +0100

    Added LongestWord.js

---
+++ FindLongestWordInString/LongestWord.js
@@ -0,0 +1,14 @@
+function findLongestWord(str) {
+	
+	var strArray = str.split(" ");
+	var strLength = [];
+	for (var i = 0; i < strArray.length; i++) {
+		
+		strLength.push(strArray[i].length);
+		strLength.sort(function(a, b){return a - b;});
+		strLength.reverse();
+	}
+  return strLength[0];
+}
+
+findLongestWord("The quick brown fox jumped over the lazy dog");
