020ed6bd1a81b5c5398cf1ef82308f980fdaaa99
Israel Newham <israel.newham@gmail.com>
2018-05-17 18:51:02 +0100

    Completed Basic Alga

--- BasicAlgorithmScripts/CaesarsCipher/CaesarsCipher.js
+++ BasicAlgorithmScripts/CaesarsCipher/CaesarsCipher.js
@@ -3,22 +3,22 @@ Caesar cipher, also known as a shift cipher.
 In a shift cipher the meanings of the letters are shifted by some set amount. */
 
 function rot13(str) {
-  var result = str.split('');
-  var utf16 = "";
+	var result = str.split('');
+	var utf16 = "";
 
-  for(var i=0;i<str.length;++i){
-	if(result[i].charCodeAt(0) >= 65 || result[i].charCodeAt(0) === 80){
- 		utf16 += String.fromCharCode(result[i].charCodeAt(0)-13);
- 		console.log(String.fromCharCode(result[i].charCodeAt(0)-13));
+	for(var i=0;i<str.length;++i){
+		index = result[i].charCodeAt(0);
+		console.log("result["+i+"] = "+index+" "+"("+ String.fromCharCode(index)+")");
+		if((index-13) >= 65){
+			utf16 += String.fromCharCode((index-13));
+		}else if((index-13) >= 52){
+			utf16 += String.fromCharCode((index-13)+26);
+		}else{
+			utf16 += String.fromCharCode(index);
+		}
 	}
-  	else
-  	{
-  	console.log("space");
-  	utf16 += result[i];
-  	}
-  }
-return utf16;
 
+	return utf16;
 }
 
 rot13("SERR PBQR PNZC");
